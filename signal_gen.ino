4eb0a212140b0ffec30d9c6efb1c2ce83a04ac5a
Amogh Kondaji <akondaji3@gatech.edu>
2025-11-22 17:08:32 -0500

    mapping and thresholding, update volume on ulcd

--- signal_gen.ino
+++ signal_gen.ino
@@ -1,34 +1,46 @@
-#include "signal_gen.h"
 #include <esp32-hal-dac.h>
 #include "Adafruit_VL53L0X.h"
+#include "signal_gen.h"
+#include "ulcd.h"
 
 #define DAC_PIN 25
 #define POT_PIN 34
+#define MUTE_PIN 27
 
 #define C3 130
 #define C4 262
 
-#define NUM_SAMPLES 10
+#define NUM_SAMPLES 20
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 uint16_t samples[NUM_SAMPLES];
-int readIndex = 0;          // the index of the current reading
-int total = 0;              // the running total
-int average = 0;            // the average
+int readIndex = 0;
+int total = 0;
+int average = 0;
 
 uint32_t freq;
 uint32_t phase;
 uint32_t tuning_word;
+uint16_t volume;
+uint16_t pot_val;
 
-volatile uint16_t pot_val;
+uint8_t *lut;
 
 hw_timer_t *timer;
+
+// void ARDUINO_ISR_ATTR onVolumeChange()
+// {
+//   ulcd_update_volume(volume);
+// }
 
 void ARDUINO_ISR_ATTR incFcw()
 {
   phase += tuning_word;
   // uint8_t out = (uint8_t)(amp * lut[getTableIdx(phase)]);
-  uint8_t out = (pot_val / 255.0) * lut[getTableIdx(phase)];
+  if (digitalRead(MUTE_PIN))
+    volume = 0;
+  // uint8_t out = (pot_val / 255.0) * lut[getTableIdx(phase)];
+  uint8_t out = (volume / 255.0) * sin_lut[getTableIdx(phase)];
   dacWrite(DAC_PIN, out);
 }
 
@@ -45,18 +57,22 @@ void setup() {
   phase = 0;
   tuning_word = getFreqCtrlWord(freq);
 
+  ulcd_init();
+
   Serial.println("Adafruit VL53L0X test");
   if (!lox.begin()) {
     Serial.println(F("Failed to boot VL53L0X"));
     while(1);
   }
-  // power 
-  Serial.println(F("VL53L0X API Simple Ranging example\n\n")); 
 
   timer = timerBegin(REFCLK);
   timerAttachInterrupt(timer, &incFcw);
   uint64_t prescaler = REFCLK / SAMPLE_RATE;
   timerAlarm(timer, prescaler, true, 0);
+
+  pinMode(MUTE_PIN, INPUT_PULLDOWN);
+
+  lut = sin_lut;
 }
 
 void loop() {
@@ -67,25 +83,46 @@ void loop() {
   total = total - samples[readIndex];
     
   // Serial.print("Reading a measurement... ");
-  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
-
-  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
-    // Serial.print("Distance (mm): "); Serial.println(tuning_word);
+  lox.rangingTest(&measure, false);
+  if (measure.RangeStatus != 4) {
     samples[readIndex] = measure.RangeMilliMeter;
   } else {
     // Serial.println(" out of range ");
     samples[readIndex] = 30;
   }
-  total += samples[readIndex];
+
+  uint16_t sample = samples[readIndex];
+  total += sample;
   readIndex = (readIndex + 1) % NUM_SAMPLES;
   average = total / NUM_SAMPLES;
 
-  freq = map(samples[readIndex], 30, 300, C3, C4);
-  uint16_t sample = samples[readIndex]
-  freq = constrain(freq, C3, C4);
+  uint8_t freq_index = 0;
+  if (sample <= 30)
+  {
+    // freq_index = 0;
+    freq = C3;
+    volume = 0;
+  }
+  else if (sample <= 60)
+  {
+    freq = C4;
+    volume = pot_val;
+  }
+  else if (sample >= 300)
+  {
+    freq_index = C4;
+    volume = pot_val;
+  }
+  else 
+  {
+    // float width = (300 - 30) / SCALE_SIZE;
+    // freq_index = (uint16_t)((sample - 30) / width);
+    freq = map(sample, 60, 300, C3, C4) << 1;
+    volume = pot_val;
+  }
+  // freq = c3_scale[freq_index] << 1;
   tuning_word = getFreqCtrlWord(freq);
-  // Serial.println(phase);
 
-
+  ulcd_update_volume(volume);
   delay(10);
 }
--- ulcd.ino
+++ ulcd.ino
@@ -1,33 +1,75 @@
+#include "Goldelox_Serial_4DLib.h"
+#include "Goldelox_Const4D.h"
 #include "ulcd.h"
 
-char waveMode[10];
-int volume = 0;
+#define TX_PIN      14   // UART2 TX
+#define RX_PIN      13   // UART2 RX  2
+#define RESETLINE   4
 
-void setup() {
-  Serial.begin(115200);
+HardwareSerial DisplaySerial(1);
+Goldelox_Serial_4DLib Display(&DisplaySerial);
 
-  pinMode(BUTTON_UP,   INPUT_PULLUP);
-  pinMode(BUTTON_DOWN, INPUT_PULLUP);
-  pinMode(POT_PIN,     INPUT);
+void ulcd_init()
+{
+  pinMode(RESETLINE, OUTPUT);
+  digitalWrite(RESETLINE, LOW);
+  delay(100);
+  digitalWrite(RESETLINE, HIGH);
+  delay(500);
 
-  Ulcd_init();
+  DisplaySerial.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN);
+  Display.TimeLimit4D = 5000;
 
-  loadSettings(waveMode, sizeof(waveMode), &volume);
-  UlcdUI_drawWave(waveMode);
-  drawVolume(volume);
+  Display.gfx_Cls();
+  Display.txt_FontID(5);
+  Display.txt_BGcolour(BLACK);
+  Display.txt_Set(TEXT_COLOUR, WHITE);
+  Display.txt_Set(TEXT_HEIGHT, 1.7);
+  Display.txt_Set(TEXT_WIDTH, 1.7);
+
+  Display.txt_MoveCursor(1, 2); Display.putstr("Wave: ");
+  Display.txt_MoveCursor(5, 2); Display.putstr("Vol: ");
+  Display.txt_MoveCursor(9, 2); Display.putstr("Note: ");
+  Serial.println("Initialized ULCD screen");
 }
 
-void loop() {
+void ulcd_update_wave(Wave wave)
+{
+  char *buf;
+  switch (wave)
+  {
+    case SIN:
+      buf = "Sine";
+    case TRI:
+      buf = "Triangle";
+    case SAW:
+      buf = "Sawtooth";
+    case SQU:
+      buf = "Square";
+    default:
+      buf = "";
+  }
 
-  NavWave(waveMode);
-  updateVolumeFromPot();
+  Display.txt_MoveCursor(1, 7);
+  Display.putstr("     ");   // clear old
+  Display.txt_MoveCursor(1, 7);
+  Display.putstr(buf);
+}
 
+void ulcd_update_volume(uint16_t vol)
+{
+  float display_vol = (vol / 255.0) * 100;
+  char buf[16];
 
-  const char* note = "C5";
-  int freq = 523;
-  updatePitchIfChanged(note, freq);
+  if (display_vol < 10)
+    snprintf(buf, sizeof(buf), "%.2f%%  ", display_vol);
+  else if (display_vol < 100)
+    snprintf(buf, sizeof(buf), "%.2f%% ", display_vol);
+  else
+    snprintf(buf, sizeof(buf), "%.2f%%", display_vol);
 
-  // 可以考虑每隔几百毫秒再保存一次，而不是每次 loop 都写 flash
-  // saveSettings(waveMode, 当前的g_lastVolume);
-  delay(10);
+  Display.txt_MoveCursor(5, 7);
+  // Display.putstr("      ");
+  Display.txt_MoveCursor(5, 7);
+  Display.putstr(buf);
 }
